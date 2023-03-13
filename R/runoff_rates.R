4a458663ef0d0195ff09a5c327bec37482dfb983
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-03-12 18:44:21 -0700

    removed unused parameters

--- R/runoff_rates.R
+++ R/runoff_rates.R
@@ -3,17 +3,16 @@
 #' This function allows the user to input land area, fertlizer application rate, and precipitation and compute the estimated
 #' amount of water into the nearby waterbody and how run off impacts water quality.
 #' 
-#' @param runoff_coeff constant
+#' @param runoff_coeff constant based on soil type 
 #' @param nutrient_conc concentration of nitrogen in land area (mg/L)
 #' @param land_area (ha) size of land where fertilizer is applied
 #' @param application_rate kg Nitrogen/ha
-#' @param precipitaion rain (mL/24hr)
-#' @param catchment_area km^2
+#' @param precipitaion rainfall intensity (mL/24hr)
 #' @param nitrogen_max  (mg/L) maximum nitrogen level in fresh water bodies by EPA (using NJ standards)
 #' 
 #'
 #' @return runoff concentration
-#' @return water quality (high, medium low based on water quality standards) for catchment area
+
 
 #' 
 runoff_rates <- function(land_area, application_rate, precipitation){
