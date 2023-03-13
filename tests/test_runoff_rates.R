03b6ec46577626db23b870b6a981466b560a2e08
srohlfing92 <srohlfing@ucsb.edu>
2023-03-12 20:12:57 -0700

    Added Grace to HW title, deleted unused code

--- tests/test_runoff_rates.R
+++ tests/test_runoff_rates.R
@@ -6,9 +6,3 @@ test_that("runoff_rates_works", {
 
 ## run test_file("../tests/test_computes_NPV.R)
 ## test_dir("../tests") # runs all tests in test folder
-
-
-
-#ifelse(a == c(2,3), TRUE, FALSE)
-#[1] TRUE TRUE
-#> expect_equal(a, c(2,3))
