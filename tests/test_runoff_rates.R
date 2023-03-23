4d05752e31008544b09d9e10851d531426655b96
srohlfing92 <srohlfing@ucsb.edu>
2023-03-22 18:56:48 -0700

    tests work for list outputs now. still need to add in error checking in the function

--- tests/test_runoff_rates.R
+++ tests/test_runoff_rates.R
@@ -1,7 +1,12 @@
 test_that("runoff_rates_works", {
 
-  expect_equal(runoff_rates(land_area = 25, application_rate = 20, precipitation = 0), 0)
-  expect_equal(runoff_rates(land_area = 100, application_rate = 0, precipitation = 20), 0)
+#  expect_equal(runoff_rates(land_area = 25, application_rate = 20, precipitation = 0), runoff_rate = 0)
+#  expect_equal(runoff_rates(land_area = 100, application_rate = 0, precipitation = 20), runoff_rate = 0)
+ 
+  expect_that(runoff_rates(land_area = 25, application_rate = 20, precipitation = 0)$runoff_rate, equals(0))
+  expect_that(runoff_rates(land_area = 100, application_rate = 0, precipitation = 20)$runoff_rate, equals(0)) 
+  expect_that(runoff_rates(land_area = 100, application_rate = 0, precipitation = 20)$nutrient_conc, equals(0))
+  
 })
 
 ## run test_file("../tests/test_computes_NPV.R)
