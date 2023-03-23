38067f5dd8645913f8fa5a0a0502a42f6a407ac4
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-03-22 17:39:29 -0700

    added for loop and return two outputs from runoff function

--- R/runoff_rates.R
+++ R/runoff_rates.R
@@ -3,24 +3,27 @@
 #' This function computes the estimated amount fertilizer runoff for a given the land area, fertilizer application rate, and precipitation.
 #' 
 #' 
-#' @param runoff_coeff constant based on soil type 
-#' @param nutrient_conc concentration of nitrogen in land area (mg/L)
+#' @param runoff_coeff constant based on soil type 0.2
+#' @param nutrient_conc concentration of nitrogen in land area (kg)
 #' @param land_area (ha) size of land where fertilizer is applied
 #' @param application_rate kg Nitrogen/ha
 #' @param precipitaion rainfall intensity (mL/24hr)
 #' @param nitrogen_max  (mg/L) maximum nitrogen level in fresh water bodies by EPA (using NJ standards)
 #' 
 #'
-#' @return runoff concentration
+#' @return runoff concentration (mg/L)
 
 
 #' 
 runoff_rates <- function(land_area, application_rate, precipitation){
-  runoff_coeff = 0.2
-  nutrient_conc = (application_rate*land_area)/2
+  land_area[land_area < 1]  <- NA # return NA for land area smaller than 1 ha
+  
+  #land_area[land_area < 1] <- NA # remove any rows with areas less than 1
+  nutrient_conc = (application_rate*land_area)/ 2
+  runoff_rate = (nutrient_conc*0.2*precipitation)/ 1000
 
-  runoff_rate = (nutrient_conc*runoff_coeff*precipitation)/ 1000
 
-  return(runoff_rate)
+
+  return(list(runoff_rate=runoff_rate, nutrient_conc=nutrient_conc))
 
 }
